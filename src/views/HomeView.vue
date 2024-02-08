be78d5fc6a13b308b8033f8c4755b8297c74ec01
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-02-08 23:17:26 +0700

    Начальная реализация каталога

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <h1 class="catalog" @click="getCatalogProduct">Каталог</h1>
+  <div v-for="product in products" :key="product.id" class="catalog">
+    <div class="product">
+      <p>{{ product.name }}</p>
+      <p>{{ product.description }}</p>
+      <p>{{ product.price }} руб.</p>
+      <button>В корзину</button>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
-
+  name: 'HomeView',
+  data() {
+    return {
+      url: "https://jurapro.bhuser.ru/api-shop/products",
+      products: [],
+    }
+  },
+  methods:{
+    async getCatalogProduct(){
+      const response = await fetch(this.url,{
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+        const result = await response.json();
+        this.products = result.data
+        console.log('Result: ', result)
+      } else {
+        this.error = "Ошибка";
+        console.error(this.error);
+      }
+    }
+  }
 }
 </script>
